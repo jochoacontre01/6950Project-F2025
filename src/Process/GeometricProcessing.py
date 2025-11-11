2f1857cc50f8615fc63bababb0418df438a913c5
Jesus Ochoa <jochoacontre@mun.ca>
2025-11-11 15:54:41 -0330

    Added coordinates preprocessing

--- src/Process/GeometricProcessing.py
+++ src/Process/GeometricProcessing.py
@@ -3,3 +3,12 @@
 def crop(ds, latmin, latmax, lonmin, lonmax):
 	ds_cropped = ds.sel(lon=slice(lonmin, lonmax), lat=slice(latmin, latmax))
 	return ds_cropped
+
+def adjust_coords(ds):
+	lon = ds.coords['lon']
+	adjusted_lon = ((lon + 180) % 360) - 180
+
+	ds = ds.assign_coords(lon=adjusted_lon)
+	ds = ds.sortby(['lat', 'lon'], ascending=True)
+
+	return ds
