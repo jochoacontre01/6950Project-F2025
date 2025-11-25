80bab40827ba0e6eef1eec9f59fa912f3c1027b7
Jesus Ochoa <jochoacontre@mun.ca>
2025-11-25 09:51:12 -0330

    Modified filenames to plot different variables

--- src/plot.py
+++ src/plot.py
@@ -20,6 +20,9 @@ matplotlib.rcParams.update({"font.size": 16})
 # ! Point of analysis (about central Newfoundland) as (lat, lon)
 interest_point = [55.37437902920724, -60.67567886857576]
 buffer_deg = 15.0
+savepath = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), "docs/6950_project_latex/Fig"
+)
 
 
 def load_preprocess(path, center_point, buffer, var):
@@ -83,8 +86,7 @@ scenario_370 = match_scenario(ds_path_370).format()
 
 scenarios = [scenario_historical, scenario_245, scenario_370]
 
-ds_hist_245 = xr.concat([ds_historical, ds_245], dim='time')
-
+ds_hist_245 = xr.concat([ds_historical, ds_245], dim="time")
 
 
 # ! ###################################
@@ -133,16 +135,42 @@ toplot_370 = PlotObject(difference, var_370)
 # ! ###################################
 # ! MAP PLOTTING
 
+vmin = np.amin(np.stack([toplot_historical.data.values, toplot_245.data.values, toplot_370.data.values]).flatten())
+vmax = np.amax(np.stack([toplot_historical.data.values, toplot_245.data.values, toplot_370.data.values]).flatten())
+vmin = -vmax
+
 toplot_historical.create_map(
-    scenario="Historical", cpoint=interest_point, buffer=buffer_deg
+    scenario="Historical",
+    cpoint=interest_point,
+    buffer=buffer_deg,
+    # save_to=savepath + "/map_temp_hist.png",
+    
+    vmin=vmin,
+    vmax=vmax,
+    interpolation='bicubic',
+    interpolation_stage='rgba'
 )
 
 toplot_245.create_map(
-    scenario="SSP2-4.5", cpoint=interest_point, buffer=buffer_deg
+    scenario="SSP2-4.5",
+    cpoint=interest_point,
+    buffer=buffer_deg,
+    # save_to=savepath + "/map_temp_245.png",
+    vmin=vmin,
+    vmax=vmax,
+    interpolation='bicubic',
+    interpolation_stage='rgba'
 )
 
 toplot_370.create_map(
-    scenario="SSP3-7.0", cpoint=interest_point, buffer=buffer_deg
+    scenario="SSP3-7.0",
+    cpoint=interest_point,
+    buffer=buffer_deg,
+    # save_to=savepath + "/map_temp_370.png",
+    vmin=vmin,
+    vmax=vmax,
+    interpolation='bicubic',
+    interpolation_stage='rgba'
 )
 
 # map_plot(toplot_370, scenario_370)
@@ -150,69 +178,58 @@ toplot_370.create_map(
 # ! -------------
 # ! Time series plot of snow depth change
 
-ds_historical_filtered = (
-    ds_historical.resample(time="QS-DEC")
-    .mean(
-        
-    )[::4]
-    .mean( dim=["lat", "lon"])
-)
-ds_245_filtered = (
-    ds_245.resample(time="QS-DEC")
-    .mean(
-        
-    )[::4]
-    .mean( dim=["lat", "lon"])
-)
 ds_hist_245_filtered = (
-    ds_hist_245.resample(time="QS-DEC")
-    .mean(
-        
-    )[::4]
-    .mean( dim=["lat", "lon"])
+    ds_hist_245.resample(time="QS-DEC").mean()[::4].mean(dim=["lat", "lon"])
 )
-ds_370_filtered = (
-    ds_370.resample(time="QS-DEC")
-    .mean(
-        
-    )[::4]
-    .mean( dim=["lat", "lon"])
+ds_historical_filtered = (
+    ds_historical.resample(time="QS-DEC").mean()[::4].mean(dim=["lat", "lon"])
 )
+ds_245_filtered = ds_245.resample(time="QS-DEC").mean()[::4].mean(dim=["lat", "lon"])
+ds_370_filtered = ds_370.resample(time="QS-DEC").mean()[::4].mean(dim=["lat", "lon"])
 
 years_hist_245 = ds_hist_245_filtered.time.dt.year
 years_historical = ds_historical_filtered.time.dt.year
 years_245 = ds_245_filtered.time.dt.year
 years_370 = ds_370_filtered.time.dt.year
 
-years_245_n = np.arange(min(years_245), max(years_245)+1)
+years_245_n = np.arange(min(years_245), max(years_245) + 1)
 coeff_245 = np.polyfit(years_245, ds_245_filtered, deg=5)
 trend_245 = np.polyval(coeff_245, years_245_n)
 # trend_245 = coeff_245[0]*years_245_n + coeff_245[1]
 
-years_370_n = np.arange(min(years_370), max(years_370)+1)
+years_370_n = np.arange(min(years_370), max(years_370) + 1)
 coeff_370 = np.polyfit(years_370, ds_370_filtered, deg=5)
 trend_370 = np.polyval(coeff_370, years_370_n)
 # trend_370 = coeff_370[0]*years_370_n + coeff_370[1]
 
-fig = plt.figure(figsize=(12,6))
+fig = plt.figure(figsize=(12, 6))
 
 # plt.plot(years_hist_245, ds_hist_245_filtered, "oy-", lw=1.5, label="Historical + SSP2-4.5")
-plt.plot(years_245, trend_245, "y--", lw=1.5)
-plt.plot(years_370, trend_370, "r--", lw=1.5)
-plt.plot(years_historical, ds_historical_filtered, "ok-", ms=5, alpha=0.5, lw=1.5, label="Historical")
-plt.plot(years_245, ds_245_filtered, "y-", alpha=0.5, lw=0.75, label="SSP2-4.5")
-plt.plot(years_370, ds_370_filtered, "r-", alpha=0.5, lw=0.75, label="SSP3-7.0")
+plt.plot(years_245, trend_245, c='orange', ls="--", lw=1.5)
+plt.plot(years_370, trend_370, c='r', ls="--", lw=1.5)
+plt.plot(
+    years_historical,
+    ds_historical_filtered,
+    "ok-",
+    ms=5,
+    alpha=0.5,
+    lw=1.5,
+    label="Historical",
+)
+plt.plot(years_245, ds_245_filtered, c="orange", alpha=0.5, lw=0.75, label="SSP2-4.5")
+plt.plot(years_370, ds_370_filtered, c='r', alpha=0.5, lw=0.75, label="SSP3-7.0")
 plt.xlabel("Year")
 
 if var_historical == "snd":
     plt.ylabel("Depth (m)")
     plt.title(f"Snow depth change in Atlantic Canada\nScenario {scenario_historical}")
 elif var_historical == "tas":
-    plt.ylabel("Tenperature (C)")
+    plt.ylabel("Temperature (C)")
     plt.title(f"Temperature change in Atlantic Canada\nScenario {scenario_historical}")
 
+plt.title("Average temperature in Atlantic Canada")
 plt.gcf().set_size_inches(8, 6)
 plt.tight_layout()
 plt.legend()
-# plt.savefig('src/Fig/snow_change_timeseries_atl_CA.png', dpi=150)
+# plt.savefig(f'{savepath}/temp_timeseries.png', dpi=150)
 plt.show()
