7c95fe3b98ebc89fb120064129c61ddfde035209
Jesus Ochoa <jochoacontre@mun.ca>
2025-11-27 10:35:54 -0330

    refactored loading function to a class with attributes

--- src/Process/GeometricProcessing.py
+++ src/Process/GeometricProcessing.py
@@ -1,4 +1,6 @@
 import xarray as xr
+import os
+from Process.StringOps import match_scenario
 
 def crop(ds, latmin, latmax, lonmin, lonmax):
 	ds_cropped = ds.sel(lon=slice(lonmin, lonmax), lat=slice(latmin, latmax))
@@ -13,20 +15,32 @@ def adjust_coords(ds):
 
 	return ds
 
-def load_preprocess(path, center_point, buffer, var):
-    bbox = [
-        center_point[0] - buffer,
-        center_point[0] + buffer,
-        center_point[1] - buffer,
-        center_point[1] + buffer,
-    ]
+class load_preprocess: 
+    
+    def __init__(self, path, center_point, buffer=0):
+        
+        self.path = path
+        self.var = os.path.basename(self.path).split("_")[0]
+        
+        scenario = match_scenario(path).format()
+        self.scenario = scenario if scenario is not None else "Historical"
+        self.bbox = [
+            center_point[0] - buffer,
+            center_point[0] + buffer,
+            center_point[1] - buffer,
+            center_point[1] + buffer,
+        ]
+        self.ds = None
+        
+        self.load()
 
-    # ! Load dataset with xarray and perform basic preprocessing
-    ds = xr.load_dataset(path, engine="h5netcdf")[var]
-    if var == "tas":
-        ds = ds - 273.15
-    ds = adjust_coords(ds)
-    ds = crop(ds, *bbox)
-    ds += 1e-15  # ? Add stability constant
-
-    return ds
+    def load(self):
+        # ! Load dataset with xarray and perform basic preprocessing
+        ds = xr.load_dataset(self.path, engine="h5netcdf")[self.var]
+        if self.var == "tas":
+            ds = ds - 273.15
+        ds = adjust_coords(ds)
+        ds = crop(ds, *self.bbox)
+        ds += 1e-15  # ? Add stability constant
+        self.ds = ds
+        return self.ds
