f8b9be15d54043d10ce6ab98cd37ca4557b0a3e6
Jesus Ochoa <jochoacontre@mun.ca>
2025-11-27 12:34:38 -0330

    Added function to created dummy data

--- src/test_extreme_events.py
+++ src/test_extreme_events.py
@@ -5,3 +5,36 @@ import pandas as pd
 from extreme_events import higher_than_average
 
 
+def synthetic_dataset(start_year, end_year, value):
+    times = pd.date_range(
+        start=f'{start_year}-01-01',
+        end=f'{end_year}-01-01',
+        freq="ME"
+    )
+    
+    lats = np.arange(50,61,1, dtype=float)
+    lons = np.arange(-40,-30,1, dtype=float)
+    
+    data = np.ones((len(times), len(lats), len(lons)), dtype=float) * value
+    
+    da = xr.DataArray(
+        data,
+        coords={
+            "time": times,
+            "lat": lats,
+            "lon": lons
+        },
+        dims=(
+            "time", "lat", "lon"
+        ),
+        name="tas"
+    )
+    
+    return da
+    
+synthetic_dataset(1950, 2000, 5)
+
+
+@pytest.mark.parametrize([
+    (2015, 2030)
+])
