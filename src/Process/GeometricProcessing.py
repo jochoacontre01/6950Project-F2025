7434500dacc8e013a2936fdf864ff6ac05058847
Jesus Ochoa <jochoacontre@mun.ca>
2025-11-27 10:03:56 -0330

    moved preprocess function to this file

--- src/Process/GeometricProcessing.py
+++ src/Process/GeometricProcessing.py
@@ -1,4 +1,4 @@
-
+import xarray as xr
 
 def crop(ds, latmin, latmax, lonmin, lonmax):
 	ds_cropped = ds.sel(lon=slice(lonmin, lonmax), lat=slice(latmin, latmax))
@@ -12,3 +12,21 @@ def adjust_coords(ds):
 	ds = ds.sortby(['lat', 'lon'], ascending=True)
 
 	return ds
+
+def load_preprocess(path, center_point, buffer, var):
+    bbox = [
+        center_point[0] - buffer,
+        center_point[0] + buffer,
+        center_point[1] - buffer,
+        center_point[1] + buffer,
+    ]
+
+    # ! Load dataset with xarray and perform basic preprocessing
+    ds = xr.load_dataset(path, engine="h5netcdf")[var]
+    if var == "tas":
+        ds = ds - 273.15
+    ds = adjust_coords(ds)
+    ds = crop(ds, *bbox)
+    ds += 1e-15  # ? Add stability constant
+
+    return ds
